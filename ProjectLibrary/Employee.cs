b973d0942cc927fecc8602eef98d76fe52af1839
ktlhape <ktlhape@iie.ac.za>
2023-10-23 16:39:03 +0200

    Added login form and employee class

---
+++ ProjectLibrary/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary
+{
+    public class Employee
+    {
+        static SqlConnection con = Connections.GetConnection();
+        public string EmployeeNo { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public double Salary { get; set; }
+        public string Password { get; set; }
+        public string EmpType { get; set; }
+
+        public Employee(string employeeNo, string firstname, string lastname, double salary, string password, string empType)
+        {
+            EmployeeNo = employeeNo;
+            Firstname = firstname;
+            Lastname = lastname;
+            Salary = salary;
+            Password = password;
+            EmpType = empType;
+        }
+
+        public static Employee GetEmployee(string empNo)
+        {
+            Employee em = null;
+            using (con)
+            {
+                string strSelect = $"SELECT * FROM Employee WHERE EmployeeNo = '{empNo}'";
+                con.Open();
+                SqlCommand cmdSelect = new(strSelect, con);
+                using (SqlDataReader rd = cmdSelect.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        em = new(rd.GetString(0), rd.GetString(1), 
+                            rd.GetString(2), rd.GetDouble(3), rd.GetString(4), 
+                            rd.GetString(5));
+                    }
+                }
+            }
+            return em;
+        }
+    }
+}
---
+++ ProjectTimeline/Views/Login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ProjectLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectTimeline.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string empNo, pass;
+            empNo = txtEmpNo.Text;
+            pass = txtPass.Password.ToString();
+
+            Employee em = Employee.GetEmployee(empNo);
+
+            if (em.EmployeeNo.Equals(empNo) && em.Password.Equals(pass))
+            {
+                MessageBox.Show($"Hello {em.Firstname} {em.Lastname}");
+            }
+            else
+            {
+                MessageBox.Show($"Employee ({empNo}) does not exist");
+            }
+
+        }
+    }
+}
